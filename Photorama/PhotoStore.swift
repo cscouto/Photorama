22b27b9a64bfe2dffda9aea4537f0c2f3ff5ed94
AutoAvaliar <cs.couto@outlook.com>
2017-06-29 15:05:26 -0300

    page 956 - chapter 23 end

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -22,6 +22,11 @@ enum PhotosResult {
     case failure(Error)
 }
 
+enum TagsResult {
+    case success([Tag])
+    case failure(Error)
+}
+
 class PhotoStore {
     let imageStore = ImageStore()
     let persistentContainer: NSPersistentContainer = {
@@ -45,17 +50,12 @@ class PhotoStore {
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
             
-            var result = self.processPhotosRequest(data: data, error: error)
-            
-            if case .success = result {
-                do {
-                    try self.persistentContainer.viewContext.save()
-                } catch let error {
-                    result = .failure(error)
-                }
-            }
-            OperationQueue.main.addOperation {
+            self.processPhotosRequest(data: data, error: error) {
+                (result) in
+                
+                OperationQueue.main.addOperation {
                 completion(result)
+                }
             }
         }
         task.resume()
@@ -91,14 +91,38 @@ class PhotoStore {
         task.resume()
     }
     
-    private func processPhotosRequest(data: Data?, error: Error?) -> PhotosResult {
+    private func processPhotosRequest(data: Data?,
+                                         error: Error?,
+                                         completion: @escaping (PhotosResult) -> Void) {
         guard let jsonData = data else {
-            return .failure(error!)
+            completion(.failure(error!))
+            return
         }
-        print("JSON \(jsonData)")
-        return FlickrAPI.photos(fromJSON: jsonData, into: persistentContainer.viewContext)
+        
+        persistentContainer.performBackgroundTask {
+            (context) in
+            let result = FlickrAPI.photos(fromJSON: jsonData, into: context)
+            
+            do {
+                try context.save()
+            } catch {
+                print("Error saving to Core Data: \(error).")
+                completion(.failure(error))
+                return
+            }
+            
+            switch result {
+            case let .success(photos):
+                let photoIDs = photos.map { return $0.objectID }
+                let viewContext = self.persistentContainer.viewContext
+                let viewContextPhotos =
+                    photoIDs.map { return viewContext.object(with: $0) } as! [Photo]
+                completion(.success(viewContextPhotos))
+            case .failure:
+                completion(result)
+            }
+        }
     }
-    
     private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
         guard
             let imageData = data,
@@ -131,5 +155,21 @@ class PhotoStore {
             }
         }
     }
+    
+    func fetchAllTags(completion: @escaping (TagsResult) -> Void) {
+        let fetchRequest: NSFetchRequest<Tag> = Tag.fetchRequest()
+        let sortByName = NSSortDescriptor(key: #keyPath(Tag.name), ascending: true)
+        fetchRequest.sortDescriptors = [sortByName]
+        let viewContext = persistentContainer.viewContext
+        viewContext.perform {
+            do {
+                let allTags = try fetchRequest.execute()
+                completion(.success(allTags))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+    }
+    
     
 }
---
+++ Photorama/TagDataSource.swift
@@ -0,0 +1,33 @@
+//
+//  TagDataSource.swift
+//  Photorama
+//
+//  Created by AutoAvaliar on 29/06/17.
+//  Copyright © 2017 CoutoCode. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TagDataSource: NSObject, UITableViewDataSource {
+    
+    var tags: [Tag] = []
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",
+                                                 for: indexPath)
+        
+        let tag = tags[indexPath.row]
+        cell.textLabel?.text = tag.name
+        
+        return cell
+    }
+    
+}
