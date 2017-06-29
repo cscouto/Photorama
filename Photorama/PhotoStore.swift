09229dd0a4d65b226dd602ad4ab77efbf2ceba7d
AutoAvaliar <cs.couto@outlook.com>
2017-06-29 10:12:35 -0300

    page 785 - chapter 20 end

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -6,8 +6,16 @@
 //  Copyright © 2017 CoutoCode. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+enum ImageResult {
+    case success(UIImage)
+    case failure(Error)
+}
+
+enum PhotoError: Error {
+    case imageCreationError
+}
 enum PhotosResult {
     case success([Photo])
     case failure(Error)
@@ -27,7 +35,25 @@ class PhotoStore {
             (data, response, error) -> Void in
             
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        task.resume()
+    }
+    
+    func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
+        
+        let photoURL = photo.remoteURL
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request) {
+            (data, response, error) -> Void in
+            
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -38,5 +64,21 @@ class PhotoStore {
         }
         
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> ImageResult {
+        guard
+            let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                // Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        
+        return .success(image)
     }
 }
